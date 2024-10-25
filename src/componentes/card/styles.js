a2bfa3321191b07ac57bc34abd304c2d15dc5271
unknown <dehjr1990@gmail.com>
2024-10-25 19:02:01 -0300

    [REFACTURE]

--- src/componentes/card/styles.js
+++ src/componentes/card/styles.js
@@ -53,6 +53,7 @@ export const CardItem = styled.div`
                 position: relative;
                 width: auto;
                 height: auto;
+                aspect-ratio: 1 / 0.5;
                 animation-duration: .5s;
                 border: solid .1rem white;
                 padding: 0rem .2rem;
